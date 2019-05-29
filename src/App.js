01d7228a2447338236aba068b02c72772dde07d3
Felipe Llerena <llerenafelipe7@gmail.com>
2019-05-29 18:08:03 -0500

    Create First Component -> 'Header'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './bootstrap.min.css';
+import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header
+          title="Admin Veterinary Patients"
+          />
+      </div>
+    );
+  }
 }
 
 export default App;
